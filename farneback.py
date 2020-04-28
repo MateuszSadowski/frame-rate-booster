7227d7f64f0c52019b181b36713e6b523a3c1300
Mateusz Sadowski <>
2020-04-28 17:24:30 +0200

    Add simple motion-compensated frame interpolation

--- farneback.py
+++ farneback.py
@@ -4,15 +4,15 @@ import numpy as np
 
 import helper
 
-video = helper.openVideo('sample.mov')
+video = helper.openVideo('output-cut-crop-downsampled.mkv')
 
 length, width, height, fps = helper.getVideoInfo(video)
 
 # use only avc1 -> H264
 # or mp4v -> MPEG4
 # with .mov, .avi, .mp4 or .mkv
-fourcc = cv.VideoWriter_fourcc(*'avc1')
-writer = cv.VideoWriter('output.mp4', fourcc, 2 * fps, (width, height))
+fourcc = cv.VideoWriter_fourcc(*'ffv1')
+writer = cv.VideoWriter('output-farneback-crop.mkv', fourcc, 2 * fps, (width, height))
 
 ret1, frame1 = video.read()
 if not ret1:
@@ -35,22 +35,42 @@ while(video.isOpened()):
         prevgray = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
         gray = cv.cvtColor(frame3, cv.COLOR_BGR2GRAY)
         flow = cv.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        cv.imshow('flow', helper.drawFlow(gray, flow))
+        # cv.imshow('flow', helper.drawFlow(gray, flow))
+        warpedFlow = flow * 0.5
+        newFrame = np.zeros((height, width, 3), np.uint8)
+        for row in range(height):
+            for col in range(width):
+                pixelVal = frame1[row, col]
+                (y, x) = (row, col) + warpedFlow[row, col]
+                x1 = min(int(round(x + 0.5)), width-1)
+                x2 = min(int(max(round(x - 0.5), 0)), width-1)
+                y1 = min(int(round(y + 0.5)), height-1)
+                y2 = min(int(max(round(y - 0.5), 0)), height-1)
+                y = min(int(round(y)), height-1)
+                x = min(int(round(x)), width-1)
+                newFrame[y, x] = pixelVal
+                if x1 < width:
+                    newFrame[y, x1] = pixelVal
+                newFrame[y, x2] = pixelVal
+                if y1 < height:
+                    newFrame[y1, x] = pixelVal
+                newFrame[y2, x] = pixelVal
+
+        # cv.imshow('Frame', newFrame)
         # cv.waitKey(0)
-        # cv.imshow('Frame', frame3)
-        if cv.waitKey(25) & 0xFF == ord('q'):
-            break
+        # if cv.waitKey(25) & 0xFF == ord('q'):
+        #     break
 
         # Write frames to new video file
-        # writer.write(frame1)
-        # writer.write(frame2)
+        writer.write(frame1)
+        writer.write(newFrame)
 
         frame1 = frame3
 
         frameCounter += 1
     else:
         # End of sequence
-        # writer.write(frame1)
+        writer.write(frame1)
         break
 
 print('\nSuccess')
