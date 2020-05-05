5e808603bac0b906024fe405313f0b0171e73556
Mateusz Sadowski <>
2020-05-05 20:55:35 +0200

    Add fill holes

--- farneback.py
+++ farneback.py
@@ -41,7 +41,6 @@ while(video.isOpened()):
         #     break
 
         warpedFlow = flow * 0.5
-
         # newFrame = np.full((height, width, 3), np.nan)
         newFrame = np.zeros((height, width, 3), np.uint8)
         for row in range(height):
@@ -49,9 +48,21 @@ while(video.isOpened()):
                 (y, x) = (row, col) + warpedFlow[row, col]
                 y = min(int(round(y)), height-1)
                 x = min(int(round(x)), width-1)
-                newFrame[y, x] = frame1[row, col]
+                newFrame[y, x] = frame3[row, col]
 
-        # holeInd = np.argwhere(np.isnan(newFrame))
+        # backFlow = cv.calcOpticalFlowFarneback(gray, prevgray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        # warpedBackFlow = backFlow * 0.5
+        # for row in range(height):
+        #     for col in range(width):
+        #         (y, x) = (row, col) + warpedBackFlow[row, col]
+        #         y = min(int(round(y)), height-1)
+        #         x = min(int(round(x)), width-1)
+        #         if newFrame[y, x].all():
+        #             newFrame[y, x] = np.mean([frame1[row, col], newFrame[y, x]], axis=0)
+        #         else:
+        #             newFrame[y, x] = frame1[row, col]
+
+        # holeInd = np.where(not newFrame.all())
 
         # for (row, col, channel) in holeInd:
         for row in range(height):
@@ -61,29 +72,35 @@ while(video.isOpened()):
                 if newFrame[row,col].all():
                     continue
 
-                p00 = np.full(3, 127, np.uint8)
-                p01 = np.full(3, 127, np.uint8)
-                p10 = np.full(3, 127, np.uint8)
-                p11 = np.full(3, 127, np.uint8)
+                # p00 = np.full(3, 127, np.uint8)
+                # p01 = np.full(3, 127, np.uint8)
+                # p10 = np.full(3, 127, np.uint8)
+                # p11 = np.full(3, 127, np.uint8)
                 # if row-1 >= 0 and col-1 >= 0 and not np.isnan(newFrame[row-1, col-1]).any():
                 d = 1
-                if row-d >= 0 and col-d >= 0 and newFrame[row-d,col-d].all():
-                    p00 = newFrame[row-d, col-d]
-                # if row+d <= height-d and col-d >= 0 and not np.isnan(newFrame[row+d, col-d]).any():
-                if row+d <= height-d and col-d >= 0 and newFrame[row+d,col-d].all():
-                    p01 = newFrame[row+d, col-d]
-                # if row-d >= 0 and col+d <= width-d and not np.isnan(newFrame[row-d, col+d]).any():
-                if row-d >= 0 and col+d <= width-d and newFrame[row-d,col+d].all():
-                    p10 = newFrame[row-d, col+d]
-                # if row+d <= height-d and col+d <= width-d and not np.isnan(newFrame[row+d, col+d]).any():
-                if row+d <= height-d and col+d <= width-d and newFrame[row+d,col+d].all():
-                    p11 = newFrame[row+d, col+d]
+                points = newFrame[max(row-d,0):min(row+d+1,height-1), max(col-d,0):min(col+d+1,width)]
+                points = points.reshape(points.shape[0] * points.shape[1], 3)
+                points = points[np.all(points != 0, axis=1)]
+                # if row-d >= 0 and col-d >= 0 and newFrame[row-d,col-d].all():
+                #     p00 = newFrame[row-d, col-d]
+                # # if row+d <= height-d and col-d >= 0 and not np.isnan(newFrame[row+d, col-d]).any():
+                # if row+d <= height-d and col-d >= 0 and newFrame[row+d,col-d].all():
+                #     p01 = newFrame[row+d, col-d]
+                # # if row-d >= 0 and col+d <= width-d and not np.isnan(newFrame[row-d, col+d]).any():
+                # if row-d >= 0 and col+d <= width-d and newFrame[row-d,col+d].all():
+                #     p10 = newFrame[row-d, col+d]
+                # # if row+d <= height-d and col+d <= width-d and not np.isnan(newFrame[row+d, col+d]).any():
+                # if row+d <= height-d and col+d <= width-d and newFrame[row+d,col+d].all():
+                #     p11 = newFrame[row+d, col+d]
 
-                points = [(row-d, col-d, p00), (row+d, col-d, p01), (row-d, col+d, p10), (row+d, col+d, p11)]
-                val = helper.bilinear_interpolation(row, col, points)
+                # points = [(row-d, col-d, p00), (row+d, col-d, p01), (row-d, col+d, p10), (row+d, col+d, p11)]
+                # val = helper.bilinear_interpolation(row, col, points)
                 # newFrame[row, col] = val
-                newFrame[row, col] = np.mean([p00, p01, p10, p11])
-                
+                # val = np.mean([p00, p01, p10, p11], axis=0)
+
+                val = np.mean(points, axis=0)
+                newFrame[row, col] = val
+                    
         # Show next frame after specified interval or press q to exit
         cv.imshow('Frame', newFrame)
         if cv.waitKey(0) & 0xFF == ord('q'):
