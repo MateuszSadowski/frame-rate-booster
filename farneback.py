3f0273e38ced2e29ba209246947f1a29e5234174
Mateusz Sadowski <>
2020-05-05 19:44:39 +0200

    Add fill holes

--- farneback.py
+++ farneback.py
@@ -16,30 +16,8 @@ length, width, height, fps = helper.getVideoInfo(video)
 # or mp4v -> MPEG4
 # with .mov, .avi, .mp4 or .mkv
 fourcc = cv.VideoWriter_fourcc(*'ffv1')
-writer = cv.VideoWriter('../output/room-no-occlusion-crop-farneback-3.mkv', fourcc, 2 * fps, (width, height))
+writer = cv.VideoWriter('../output/room-no-occlusion-crop-farneback-6.mkv', fourcc, 2 * fps, (width, height))
 
-def testColorDiff(newVal, currVal, refVal):
-    # newValLab = rgb2lab.rgb2lab(newVal)
-    # currValLab = rgb2lab.rgb2lab(currVal)
-    # refValLab = rgb2lab.rgb2lab(refVal)
-
-    newValLab = LabColor(newVal[0], newVal[1], newVal[2])
-    currValLab = LabColor(currVal[0], currVal[1], currVal[2])
-    refValLab = LabColor(refVal[0], refVal[1], refVal[2])
-
-    currDiff = delta_e_cie1976(currValLab, refValLab)
-    newDiff = delta_e_cie1976(newValLab, refValLab)
-
-    if newDiff < currDiff:
-        return newVal
-    else:
-        return currVal
-
-# lab1 = rgb2lab.rgb2lab([0.1, 0.2, 0.3])
-# lab2 = rgb2lab.rgb2lab([0.1, 3, 0.3])
-# labLab1 = LabColor(lab1[0], lab1[1], lab1[2])
-# labLab2 = LabColor(lab2[0], lab2[1], lab2[2])
-# diff = delta_e_cie2000(labLab1, labLab2)
 ret1, frame1 = video.read()
 if not ret1:
     # End of sequence
@@ -62,42 +40,54 @@ while(video.isOpened()):
         # if cv.waitKey(0) & 0xFF == ord('q'):
         #     break
 
-        prevlab = cv.cvtColor(frame1, cv.COLOR_BGR2LAB)
-        lab = cv.cvtColor(frame3, cv.COLOR_BGR2LAB)
-
         warpedFlow = flow * 0.5
+
+        # newFrame = np.full((height, width, 3), np.nan)
         newFrame = np.zeros((height, width, 3), np.uint8)
         for row in range(height):
             for col in range(width):
-                # print("{0}, {1}".format(row,col))
-                pixelVal = frame1[row, col]
-                pixelValLab = frame1[row, col]
                 (y, x) = (row, col) + warpedFlow[row, col]
-                x1 = min(int(round(x + 0.5)), width-1)
-                x2 = min(int(max(round(x - 0.5), 0)), width-1)
-                y1 = min(int(round(y + 0.5)), height-1)
-                y2 = min(int(max(round(y - 0.5), 0)), height-1)
                 y = min(int(round(y)), height-1)
                 x = min(int(round(x)), width-1)
-                newFrame[y, x] = testColorDiff(pixelValLab, newFrame[y, x], lab[y, x])
-                if x1 < width:
-                    newFrame[y, x1] = testColorDiff(pixelValLab, newFrame[y, x1], lab[y, x1])
-                    newFrame[y2, x1] = testColorDiff(pixelValLab, newFrame[y2, x1], lab[y2, x1])
-                    if y1 < height:
-                        newFrame[y1, x1] = testColorDiff(pixelValLab, newFrame[y1, x1], lab[y1, x1])
-                newFrame[y, x2] = testColorDiff(pixelValLab, newFrame[y, x2], lab[y, x2])
-                if y1 < height:
-                    newFrame[y1, x] = testColorDiff(pixelValLab, newFrame[y1, x], lab[y1, x])
-                    newFrame[y1, x2] = testColorDiff(pixelValLab, newFrame[y1, x2], lab[y1, x2])
-                newFrame[y2, x] = testColorDiff(pixelValLab, newFrame[y2, x], lab[y2, x])
-                newFrame[y2, x2] = testColorDiff(pixelValLab, newFrame[y2, x2], lab[y2, x2])
+                newFrame[y, x] = frame1[row, col]
 
-        newFrame = cv.cvtColor(newFrame, cv.COLOR_LAB2RGB)
+        # holeInd = np.argwhere(np.isnan(newFrame))
 
+        # for (row, col, channel) in holeInd:
+        for row in range(height):
+            for col in range(width):
+            # Perform bilinear interpolation, change NaNs to 0s
+                # if not np.isnan(newFrame[row,col]).all():
+                if newFrame[row,col].all():
+                    continue
+
+                p00 = np.full(3, 127, np.uint8)
+                p01 = np.full(3, 127, np.uint8)
+                p10 = np.full(3, 127, np.uint8)
+                p11 = np.full(3, 127, np.uint8)
+                # if row-1 >= 0 and col-1 >= 0 and not np.isnan(newFrame[row-1, col-1]).any():
+                d = 1
+                if row-d >= 0 and col-d >= 0 and newFrame[row-d,col-d].all():
+                    p00 = newFrame[row-d, col-d]
+                # if row+d <= height-d and col-d >= 0 and not np.isnan(newFrame[row+d, col-d]).any():
+                if row+d <= height-d and col-d >= 0 and newFrame[row+d,col-d].all():
+                    p01 = newFrame[row+d, col-d]
+                # if row-d >= 0 and col+d <= width-d and not np.isnan(newFrame[row-d, col+d]).any():
+                if row-d >= 0 and col+d <= width-d and newFrame[row-d,col+d].all():
+                    p10 = newFrame[row-d, col+d]
+                # if row+d <= height-d and col+d <= width-d and not np.isnan(newFrame[row+d, col+d]).any():
+                if row+d <= height-d and col+d <= width-d and newFrame[row+d,col+d].all():
+                    p11 = newFrame[row+d, col+d]
+
+                points = [(row-d, col-d, p00), (row+d, col-d, p01), (row-d, col+d, p10), (row+d, col+d, p11)]
+                val = helper.bilinear_interpolation(row, col, points)
+                # newFrame[row, col] = val
+                newFrame[row, col] = np.mean([p00, p01, p10, p11])
+                
         # Show next frame after specified interval or press q to exit
-        # cv.imshow('Frame', newFrame)
-        # if cv.waitKey(0) & 0xFF == ord('q'):
-        #     break
+        cv.imshow('Frame', newFrame)
+        if cv.waitKey(0) & 0xFF == ord('q'):
+            break
 
         # Write frames to new video file
         writer.write(frame1)
