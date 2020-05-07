42a24020b8e06e32667544c67a26727b3dc6c93e
Mateusz Sadowski <>
2020-05-07 12:27:56 +0200

    Refactor show image

--- farneback.py
+++ farneback.py
@@ -8,7 +8,7 @@ from colormath.color_diff import delta_e_cie2000, delta_e_cie1976
 import helper
 import rgb2lab
 
-video = helper.openVideo('../downsampled/room-no-occlusion-crop-downsampled.mkv')
+video = helper.openVideo('../downsampled/hand-occlusion-crop-downsampled.mkv')
 
 length, width, height, fps = helper.getVideoInfo(video)
 
@@ -16,7 +16,7 @@ length, width, height, fps = helper.getVideoInfo(video)
 # or mp4v -> MPEG4
 # with .mov, .avi, .mp4 or .mkv
 fourcc = cv.VideoWriter_fourcc(*'ffv1')
-writer = cv.VideoWriter('../output/room-no-occlusion-crop-farneback-6.mkv', fourcc, 2 * fps, (width, height))
+writer = cv.VideoWriter('../output/hand-occlusion-crop-farneback-2.mkv', fourcc, 2 * fps, (width, height))
 
 ret1, frame1 = video.read()
 if not ret1:
@@ -33,11 +33,9 @@ while(video.isOpened()):
         # frame2 = cv.addWeighted(frame1, 0.5, frame3, 0.5, 0.0)
         prevgray = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
         gray = cv.cvtColor(frame3, cv.COLOR_BGR2GRAY)
-        # if cv.waitKey(25) & 0xFF == ord('q'):
-        #     break
-        flow = cv.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        # cv.imshow('flow', helper.drawFlow(gray, flow))
-        # if cv.waitKey(0) & 0xFF == ord('q'):
+
+        flow = cv.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 50, 3, 5, 1.2, 0)
+        # if not helper.showImage(helper.drawFlow(gray, flow)):
         #     break
 
         warpedFlow = flow * 0.5
@@ -63,7 +61,6 @@ while(video.isOpened()):
         #             newFrame[y, x] = frame1[row, col]
 
         # holeInd = np.where(not newFrame.all())
-
         # for (row, col, channel) in holeInd:
         for row in range(height):
             for col in range(width):
@@ -72,39 +69,17 @@ while(video.isOpened()):
                 if newFrame[row,col].all():
                     continue
 
-                # p00 = np.full(3, 127, np.uint8)
-                # p01 = np.full(3, 127, np.uint8)
-                # p10 = np.full(3, 127, np.uint8)
-                # p11 = np.full(3, 127, np.uint8)
-                # if row-1 >= 0 and col-1 >= 0 and not np.isnan(newFrame[row-1, col-1]).any():
                 d = 1
                 points = newFrame[max(row-d,0):min(row+d+1,height-1), max(col-d,0):min(col+d+1,width)]
                 points = points.reshape(points.shape[0] * points.shape[1], 3)
                 points = points[np.all(points != 0, axis=1)]
-                # if row-d >= 0 and col-d >= 0 and newFrame[row-d,col-d].all():
-                #     p00 = newFrame[row-d, col-d]
-                # # if row+d <= height-d and col-d >= 0 and not np.isnan(newFrame[row+d, col-d]).any():
-                # if row+d <= height-d and col-d >= 0 and newFrame[row+d,col-d].all():
-                #     p01 = newFrame[row+d, col-d]
-                # # if row-d >= 0 and col+d <= width-d and not np.isnan(newFrame[row-d, col+d]).any():
-                # if row-d >= 0 and col+d <= width-d and newFrame[row-d,col+d].all():
-                #     p10 = newFrame[row-d, col+d]
-                # # if row+d <= height-d and col+d <= width-d and not np.isnan(newFrame[row+d, col+d]).any():
-                # if row+d <= height-d and col+d <= width-d and newFrame[row+d,col+d].all():
-                #     p11 = newFrame[row+d, col+d]
-
-                # points = [(row-d, col-d, p00), (row+d, col-d, p01), (row-d, col+d, p10), (row+d, col+d, p11)]
-                # val = helper.bilinear_interpolation(row, col, points)
-                # newFrame[row, col] = val
-                # val = np.mean([p00, p01, p10, p11], axis=0)
 
                 val = np.mean(points, axis=0)
                 newFrame[row, col] = val
                     
         # Show next frame after specified interval or press q to exit
-        cv.imshow('Frame', newFrame)
-        if cv.waitKey(0) & 0xFF == ord('q'):
-            break
+        # if not helper.showImage(newFrame):
+        #     break
 
         # Write frames to new video file
         writer.write(frame1)
